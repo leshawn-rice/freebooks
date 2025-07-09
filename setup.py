385ffb4a3114cd2008e11d420570d3bb914838c0
Leshawn Rice <leshawn.rice@outlook.com>
2025-07-09 01:19:44 -0700

    Updated package version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="freebooks",
-    version="0.1.1",
+    version="0.1.2",
     author="Leshawn Rice",
     author_email="leshawn.rice@yahoo.com",
     description=(
